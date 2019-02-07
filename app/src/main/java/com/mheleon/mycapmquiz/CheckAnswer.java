7a27e783a5a44a9aab90d53431138d43023e43fc
Maykol HERNANDEZ LEON <hmaykol@gmail.com>
2019-02-08 00:10:13 +0100

    Add RecyclerView to checkAnswer activity

---
+++ app/src/main/java/com/mheleon/mycapmquiz/CheckAnswer.java
@@ -0,0 +1,47 @@
+package com.mheleon.mycapmquiz;
+
+public class CheckAnswer {
+    private String nbQuestion;
+    private String question;
+    private String userAnswer;
+    private String correctAnswer;
+
+    public CheckAnswer(String nbQuestion, String question, String userAnswer, String correctAnswer) {
+        this.nbQuestion = nbQuestion;
+        this.question = question;
+        this.userAnswer = userAnswer;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getNbQuestion() {
+        return nbQuestion;
+    }
+
+    public void setNbQuestion(String nbQuestion) {
+        this.nbQuestion = nbQuestion;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getUserAnswer() {
+        return userAnswer;
+    }
+
+    public void setUserAnswer(String userAnswer) {
+        this.userAnswer = userAnswer;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+}
--- app/src/main/java/com/mheleon/mycapmquiz/ResultsActivity.java
+++ app/src/main/java/com/mheleon/mycapmquiz/ResultsActivity.java
@@ -5,27 +5,33 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.realm.Realm;
 
 public class ResultsActivity extends AppCompatActivity {
 
-    @BindView(R.id.score)
-    TextView score;
+    @BindView(R.id.score) TextView score;
+    @BindView(R.id.tryAgainButton) Button tryAgainButton;
 
     int questionNumber; // Corresponde al numero de la pregunta, no al index del arreglo
     ArrayList<String> userAnswers = new ArrayList<>();
     ArrayList<Integer> questionsIndex;
-    ArrayList<Boolean> resultats = new ArrayList<>();
+    ArrayList<Boolean> results = new ArrayList<>();
     int res = 0;
+    ArrayList<CheckAnswer> answerList = new ArrayList<>();
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +63,32 @@ public class ResultsActivity extends AppCompatActivity {
         Log.d("currentQuest", Integer.toString(questionNumber));
 
         getScore();
-        setTitle("Your score: " + Integer.toString(res)+ "/10");
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        // mRecyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        CheckAnswerAdapter ca = new CheckAnswerAdapter(answerList);
+        mRecyclerView.setAdapter(ca);
+
+/*
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new CheckAnswerAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+*/
+
+
     }
 
     public void getScore() {
@@ -69,15 +100,35 @@ public class ResultsActivity extends AppCompatActivity {
             capmQA = realm.where(CapmQA.class).equalTo("id", questionsIndex.get(i)).findFirst();
             Log.d("answer=result", userAnswers.get(i) + "==" + capmQA.getAnswer());
             if (userAnswers.get(i).equals(capmQA.getAnswer())) {
-                resultats.add(true);
+                results.add(true);
                 res++;
             } else {
-                resultats.add(false);
+                results.add(false);
             }
+            answerList.add(new CheckAnswer("Question " + Integer.toString(i+1) + ".", capmQA.getQuestion(), optionToAnswerText(capmQA, userAnswers.get(i)), optionToAnswerText(capmQA, capmQA.getAnswer())));
 
         }
         realm.close();
+
+        score.setText("Your score: " + Integer.toString(res) + "/10");
     }
 
+    public String optionToAnswerText(CapmQA capmQA, String option) {
+        switch (option) {
+            case "a":
+                return "A. " + capmQA.getA();
+            case "b":
+                return "B. " + capmQA.getB();
+            case "c":
+                return "C. " + capmQA.getC();
+            default:
+                return "D. " + capmQA.getD();
+        }
+    }
 
+    @OnClick(R.id.tryAgainButton)
+    public void tryAgainButton() {
+        Intent intent = new Intent(ResultsActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
 }
